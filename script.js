fe47e4f45e6a4cc6acf5c1baf4d193ddabfd1454
mmmeqqq <bew.bussayakorn@gmail.com>
2024-05-14 14:05:46 +0700

    done

---
+++ script.js
@@ -0,0 +1,34 @@
+//exe1.1
+function addNumber(a,b) 
+{
+    return a + b;
+}
+console.log(addNumber(5,3))
+
+
+function subtrackNumber(a,b) 
+{
+    return a - b;
+}
+console.log(subtrackNumber(8,4))
+
+function multiplyNumber(a,b) 
+{
+    return a * b;
+}
+console.log(multiplyNumber(7,3))
+
+function divideNumber(a,b) 
+{
+    return a / b;
+}
+console.log(divideNumber(54,9))
+
+
+
+//exe 1.2
+function info(firstName,location,hobby ){
+    console.log("Hi, my name is " + firstName + " I live in " + location + " and enjoy "+hobby);
+}
+info('Bussayakorn','Bkk','play sport')
+
